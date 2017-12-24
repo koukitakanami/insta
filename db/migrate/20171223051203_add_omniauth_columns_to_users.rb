d7038dbf53307ac4a8e201ef07bb784c199725a6
koukitakanami <takanami02829@gmail.com>
2017-12-24 11:32:35 +0000

    mig

--- db/migrate/20171223051203_add_omniauth_columns_to_users.rb
+++ db/migrate/20171223051203_add_omniauth_columns_to_users.rb
@@ -1,5 +1,5 @@
 class AddOmniauthColumnsToUsers < ActiveRecord::Migration
-    def change
+  def change
     add_column :users, :uid, :string, null: false, default: ""
     add_column :users, :provider, :string, null: false, default: ""
     add_column :users, :image_url, :string
